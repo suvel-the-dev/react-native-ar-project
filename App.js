0a60d99e73a0f17aa8fa0255e312463b4a093bd8
suvel-the-dev <suvelrathneswar08@gmail.com>
2022-03-07 23:03:35 +0530

    Fixed Branch

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import InitialARScreen from './src/screens/InitialARScreen';
+import SplashScreen from './src/screens/SplashScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          <Stack.Screen name="InitialARScreen" component={InitialARScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+export default App;
